f3f77bd33df12ff4156a82fccc3e62fa72bd2987
NaraeJang <j.naaaarae@gmail.com>
2023-10-17 15:26:45 -0400

    dark mode -javascript check

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,16 +2,28 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  console.log(prefersDarkMode);
+  return prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("cat");
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
 
-    document.body.classList.toggle("dark-theme", newDarkTheme);
+    // document.body.classList.toggle("dark-theme", newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <AppContext.Provider
